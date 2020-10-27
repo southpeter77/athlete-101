febf9e4071cd55fc0c792375a15f01d91e7d81bf
Peter <southpeter77@gmail.com>
2020-10-27 00:09:01 -0400

    making sign in

--- athlete101/src/App.js
+++ athlete101/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HomePage from "./components/HomePage"
+import Login from "./components/LogIn"
 
 function App() {
   return (
+    <>
    <HomePage></HomePage>
+   <Login></Login>
+   </>
   );
 }
 
--- athlete101/src/components/HomePage.js
+++ athlete101/src/components/HomePage.js
@@ -2,23 +2,9 @@ import React, {useState} from "react";
 
 
 const HomePage = () => {
-const doFetch = async () => {
-    const response = await fetch("http://localhost:8000/api/user/signup", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({email:'wow'})
-    })
-    const jsonedData = await response.json();
-    console.log(jsonedData)
-}
 
     return (
-        <>
-        <h1>welcome to front end</h1>
-        <button onClick={doFetch}>fetchCall</button>
-        </>
+<h1>hi</h1>
     )
 }
 
