93fb5a84362b4efd2be7c81d870278b796af7146
Peter <southpeter77@gmail.com>
2020-10-30 23:00:16 -0400

    switched config

--- athlete101/src/config.js
+++ athlete101/src/config.js
@@ -1,1 +1,9 @@
-export const apiUrl = `http://localhost:8000/api`
+// export const apiUrl = `http://localhost:8000/api`
+
+module.exports ={
+    apiUrl:
+        process.env.NODE_ENV === "development" 
+          ? "https://athlete101.herokuapp.com/api"
+        : "http://localhost:8000/api"
+      
+}
